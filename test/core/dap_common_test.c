2d1f41239afc2b8f87919606521a292114fab7fd
Andrey <littletux89@gmail.com>
2018-07-17 20:57:00 +0300

    Finished the test

--- test/core/dap_common_test.c
+++ test/core/dap_common_test.c
@@ -1,11 +1,10 @@
 #include "dap_common_test.h"
 
-static const int INT_VAL = 10;
-static const char * CORRECT_VALUE = "10";
-
 void test_put_int() {
+    const int INT_VAL = 10;
+    const char * EXPECTED_RESULT = "10";
     char * result_arr = itoa(INT_VAL);
-    assert(strcmp(result_arr, CORRECT_VALUE) == 0 && "test_put_int failed");
+    assert(strcmp(result_arr, EXPECTED_RESULT) == 0 && "test_put_int failed");
 }
 
 void dap_common_test_run() {
